978c0595afb105391a704b16b9959dbacf389949
JustChris <chris.cajas.m@gmail.com>
2013-01-31 15:51:41 -0600

    Re-arranged slides/thumbnails, added flashing effect

--- slider.php
+++ slider.php
@@ -29,26 +29,36 @@
 			<div class="bannercontainer">
 					<div class="banner">
 						<ul>
+							<!-- Make it your moment -->
+							<li data-transition="fade" data-delay="2500" data-masterspeed="0">
+								
+								<img src="images/slides/image14.jpg"/>
+								<div class="caption sfl" data-x="350" data-y="110" data-speed="900" data-start="500" data-easing="easeOutBack"><img src="images/slides/caption-moment-1.png"/></div>
+								<div class="caption sfl" data-x="515" data-y="160" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-moment-2.png"/></div>
+							</li>
 					
-							<!-- basic image flashing -->			
-							<li data-transition="fade" data-delay="700" data-masterspeed="0">
+							<!-- basic image flashing -->		
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
 								<img src="images/slides/image4.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
 							</li>
 							
-							<!-- basic image flashing -->
-							<li data-transition="fade" data-delay="700" data-masterspeed="0">
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
 								<img src="images/slides/image5.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
 							</li>
 						
 							<!-- first set of thumbnails -->
-							<li data-transition="fade" data-delay="2000" data-masterspeed="10">
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-delay="2000" data-masterspeed="100">
 								
 								<img src="images/slides/image1.jpg"/>
-                                <div class="caption white-bg randomrotate lfr" data-x="480" data-y="145" data-speed="300" data-start="200" data-easing="easeOutBack"><img src="images/slides/image1-t.jpg"></div>
-                                <div class="caption white-bg randomrotate lfr" data-x="580" data-y="130" data-speed="300" data-start="400" data-easing="easeOutBack"><img src="images/slides/image2-t.jpg"></div>
-                                <div class="caption white-bg randomrotate lfr" data-x="710" data-y="145" data-speed="300" data-start="600" data-easing="easeOutBack"><img src="images/slides/image3-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="480" data-y="145" data-speed="300" data-start="200" data-easing="easeOutBack">
+									<img src="images/slides/image1-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="600" data-y="130" data-speed="300" data-start="400" data-easing="easeOutBack">
+									<img src="images/slides/image5-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="740" data-y="145" data-speed="300" data-start="600" data-easing="easeOutBack">
+									<img src="images/slides/image8-t.jpg"></div>
 							</li>
 							
 							<!-- Want your tux for free? -->
@@ -58,31 +68,34 @@
                                 <div class="caption randomrotate lft" data-x="20" data-y="10" data-speed="300" data-start="500" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-1.png"></div>
                                 <div class="caption randomrotate lfl" data-x="110" data-y="65" data-speed="300" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-2.png"></div>
 							</li>
-							
-							<li data-transition="fade" data-slotamount="0" data-masterspeed="0">
-								<img src="images/slides/image9.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
+
+							<!-- basic image flashing -->							
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image10.jpg"/>
 							</li>
 							
-							<li data-transition="fade" data-slotamount="0" data-masterspeed="0">
-								<img src="images/slides/image5.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image9.jpg"/>
 							</li>
 							
 							<!-- second set of thumbnails -->
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
 							<li data-transition="fade" data-delay="2200" data-masterspeed="0">
 								
 								<img src="images/slides/image8.jpg"/>
                                 <div class="caption white-bg randomrotate lfr" data-x="350" data-y="125" data-speed="300" data-start="200" data-easing="easeOutBack">
-									<img src="images/slides/image4-t.jpg"></div>
+									<img src="images/slides/image2-t.jpg"></div>
                                 <div class="caption white-bg randomrotate lfr" data-x="470" data-y="160" data-speed="300" data-start="400" data-easing="easeOutBack">
-									<img src="images/slides/image5-t.jpg"></div>
+									<img src="images/slides/image4-t.jpg"></div>
                                 <div class="caption white-bg randomrotate lfr" data-x="600" data-y="115" data-speed="300" data-start="600" data-easing="easeOutBack">
 									<img src="images/slides/image6-t.jpg"></div>
-                                <div class="caption white-bg randomrotate lfr" data-x="730" data-y="155" data-speed="300" data-start="800" data-easing="easeOutBack">
-									<img src="images/slides/image10-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="730" data-y="145" data-speed="300" data-start="800" data-easing="easeOutBack">
+									<img src="images/slides/image7-t.jpg"></div>
 							</li>
-							
+		
+							<!-- Make it a night to remember -->					
 							<li data-transition="fade" data-delay="2500" data-masterspeed="0">
 								
 								<img src="images/slides/image13.jpg"/>
@@ -90,7 +103,7 @@
 								<div class="caption sfl" data-x="125" data-y="210" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-remember-2.png"/></div>
 							</li>
 							
-							<!-- second caption -->
+							<!-- We match every color and every mood -->
 							<li data-transition="fade" data-delay="4000" data-masterspeed="0">
 								
 								<img src="images/slides/image3.jpg"/>
@@ -99,17 +112,34 @@
 								<div class="caption sfr"  data-x="630" data-y="195" data-speed="900" data-start="1100" data-easing="easeOutBack"><img src="images/slides/caption-we-match-3.png"/></div>
 								<div class="caption sfr"  data-x="690" data-y="240" data-speed="900" data-start="1300" data-easing="easeOutBack"><img src="images/slides/caption-we-match-4.png"/></div>
 							</li>
-							
-							<!-- third set of thumbnails -->
-							<li data-transition="fade" data-delay="2000" data-masterspeed="0">
-								
-								<img src="images/slides/image14.jpg"/>
-                                <div class="caption white-bg randomrotate lfr" data-x="380" data-y="140" data-speed="300" data-start="200" data-easing="easeOutBack"><img src="images/slides/image7-t.jpg"></div>
-                                <div class="caption white-bg randomrotate lfr" data-x="500" data-y="125" data-speed="300" data-start="400" data-easing="easeOutBack"><img src="images/slides/image8-t.jpg"></div>
-                                <div class="caption white-bg randomrotate lfr" data-x="620" data-y="160" data-speed="300" data-start="600" data-easing="easeOutBack"><img src="images/slides/image9-t.jpg"></div>
+
+							<!-- basic image flashing -->							
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image1.jpg"/>
 							</li>
 							
-							<!-- third caption -->
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image7.jpg"/>
+							</li>
+
+							<!-- third set of thumbnails -->
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-delay="2200" data-masterspeed="0">
+								
+								<img src="images/slides/image11.jpg"/>
+                                <div class="caption white-bg randomrotate lfr" data-x="380" data-y="160" data-speed="300" data-start="200" data-easing="easeOutBack">
+									<img src="images/slides/image9-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="500" data-y="145" data-speed="300" data-start="400" data-easing="easeOutBack">
+									<img src="images/slides/image10-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="620" data-y="180" data-speed="300" data-start="600" data-easing="easeOutBack">
+									<img src="images/slides/image11-t.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="740" data-y="155" data-speed="300" data-start="800" data-easing="easeOutBack">
+									<img src="images/slides/image12-t.jpg"></div>
+							</li>
+							
+							<!-- Save $$$ on your tux right now -->
 							<li data-transition="fade" data-delay="2500" data-masterspeed="0">
 								
 								<img src="images/slides/image6.jpg"/>
@@ -117,7 +147,19 @@
 								<div class="caption sfl" data-x="125" data-y="130" data-speed="900" data-start="800" data-easing="easeOutBack"><img src="images/slides/caption-savemoney-2.png"/></div>
 							</li>
 							
-							<!-- fourth caption -->
+							<!-- basic image flashing -->							
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image4.jpg"/>
+							</li>
+							
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
+							<li data-transition="fade" data-masterspeed="0">
+								<img src="images/slides/image9.jpg"/>
+							</li>
+							
+							<!-- We match every color and every mood -->
+							<li data-transition="fade" data-delay="50" data-masterspeed="50"><img src="images/slides/blank.png"/></li>	
 							<li data-transition="fade" data-delay="4000" data-masterspeed="0">
 								
 								<img src="images/slides/image7.jpg"/>
@@ -127,54 +169,52 @@
 								<div class="caption sfr"  data-x="690" data-y="240" data-speed="900" data-start="1300" data-easing="easeOutBack"><img src="images/slides/caption-we-match-4.png"/></div>
 							</li>
 
-							
+							<!-- Want your tux for free? -->
 							<li data-transition="fade" data-delay="5000" data-slotamount="0" data-masterspeed="400">
 								<img src="images/slides/image12.jpg"/>
-                                <div class="caption lfl" data-x="130" data-y="200" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-1.png"></div>
-                                <div class="caption lfb" data-x="500" data-y="200" data-speed="900" data-start="1500" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-2.png"></div>
-							</li>
-							
+                                <div class="caption lfl" data-x="290" data-y="270" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-1.png"></div>
+                                <div class="caption lfb" data-x="652" data-y="270" data-speed="900" data-start="1500" data-easing="easeOutBack"><img src="images/slides/caption-free-tux-2.png"></div>
+							</li>							
 						</ul>
 					</div>
 				</div>
-				
+								
 			<script type="text/javascript">
+			
+				//jQuery.noConflict();
+			
+				jQuery(document).ready(function() {
 
-				var tpj=jQuery;
-				tpj.noConflict();
+				if (jQuery.fn.cssOriginal!=undefined)
+					jQuery.fn.css = tpj.fn.cssOriginal;
 
-				tpj(document).ready(function() {
+				jQuery('.banner').revolution(
+					{
+						delay: 600,
+						startheight: 307,
+						startwidth: 887,
 
-				if (tpj.fn.cssOriginal!=undefined)
-					tpj.fn.css = tpj.fn.cssOriginal;
+						hideThumbs: 200,
 
-					tpj('.banner').revolution(
-						{
-							delay: 700,
-							startheight:307,
-							startwidth:887,
+						thumbWidth: 100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+						thumbHeight: 50,
+						thumbAmount: 5,
 
-							hideThumbs:200,
+						navigationType:"none",					//bullet, thumb, none, both		(No Thumbs In FullWidth Version !)
+						navigationArrows:"none",		//nexttobullets, verticalcentered, none
+						navigationStyle:"round",				//round,square,navbar
 
-							thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
-							thumbHeight:50,
-							thumbAmount:5,
+						touchenabled:"on",						// Enable Swipe Function : on/off
+						onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
 
-							navigationType:"none",					//bullet, thumb, none, both		(No Thumbs In FullWidth Version !)
-							navigationArrows:"none",		//nexttobullets, verticalcentered, none
-							navigationStyle:"round",				//round,square,navbar
+						navOffsetHorizontal:0,
+						navOffsetVertical:20,
 
-							touchenabled:"on",						// Enable Swipe Function : on/off
-							onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
+						shadow:1,								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
+						fullWidth:"off"							// Turns On or Off the Fullwidth Image Centering in FullWidth Mode
 
-							navOffsetHorizontal:0,
-							navOffsetVertical:20,
-
-							shadow:1,								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows  (No Shadow in Fullwidth Version !)
-							fullWidth:"off"							// Turns On or Off the Fullwidth Image Centering in FullWidth Mode
-
-						});
 					});
+				});
 
 			</script>
 
