29c5bb490932db4f70ebf04f28f58be275a5b4fd
JustChris <chris.cajas.m@gmail.com>
2013-01-31 11:13:59 -0600

    Added thumbnail images

--- slider.php
+++ slider.php
@@ -29,161 +29,120 @@
 			<div class="bannercontainer">
 					<div class="banner">
 						<ul>
-							
-							<!-- box slide -->
-							<li data-transition="fade" data-slotamount="5" data-delay="1500" data-masterspeed="10">
-								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image1.jpg"/></div>
+						
+							<!-- first set of thumbnails -->
+							<li data-transition="fade" data-delay="2000" data-masterspeed="10">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0"><img src="images/slides/image1.jpg"/></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="400" data-y="140" data-speed="300" data-start="200" data-easing="easeOutBack"><img src="images/slides/p1.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="560" data-y="140" data-speed="300" data-start="400" data-easing="easeOutBack"><img src="images/slides/p2.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="720" data-y="140" data-speed="300" data-start="600" data-easing="easeOutBack"><img src="images/slides/p3.jpg"></div>
 							</li>
 							
 							<!-- basic image flashing -->			
-							<li data-transition="fade" data-slotamount="0" data-delay="700" data-masterspeed="0">
+							<li data-transition="fade" data-delay="700" data-masterspeed="0">
 								<img src="images/slides/image4.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="200" data-start="0">&nbsp;</div>
+								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
 							</li>
 							
 							<!-- basic image flashing -->
-							<li data-transition="fade" data-slotamount="0" data-delay="700" data-masterspeed="0">
+							<li data-transition="fade" data-delay="700" data-masterspeed="0">
 								<img src="images/slides/image5.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="200" data-start="0">&nbsp;</div>
+								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
 							</li>
 							
 							<!-- Want your tux for free? -->
-							<li data-transition="fade" data-delay="4000" data-masterspeed="0">
-								<img src="images/slides/image2.jpg"/>
-								<div class="caption fade" data-x="0" data-y="0" data-speed="100" data-start="0">&nbsp;</div>
-                                <div class="caption randomrotate lft" data-x="20" data-y="10" data-speed="300" data-start="1000" data-easing="easeOutBack"><img src="images/slides/image2_caption-1.png"></div>
-                                <div class="caption randomrotate lfl" data-x="110" data-y="65" data-speed="300" data-start="1200" data-easing="easeOutBack"><img src="images/slides/image2_caption-2.png"></div>
-							</li>
-							
-							<li data-transition="fade" data-slotamount="0" data-masterspeed="700">
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0"><img src="images/slides/image8.jpg"/></div>
-							</li>
-							
-							<li data-transition="fade" data-slotamount="0" data-masterspeed="700">
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0"><img src="images/slides/image9.jpg"/></div>
-							</li>
-							
-							<!-- new transition -->
-							<li data-transition="fade" data-delay="5000" data-masterspeed="500" data-thumb="images/thumbs/regular_thumb4.jpg">
+							<li data-transition="fade" data-delay="3000" data-masterspeed="0">
 								
-								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0"><img src="images/slides/image3.jpg"/></div>
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image2.jpg"/></div>
+                                <div class="caption randomrotate lft" data-x="20" data-y="10" data-speed="300" data-start="700" data-end="2800" data-easing="easeOutBack"><img src="images/slides/image2_caption-1.png"></div>
+                                <div class="caption randomrotate lfl" data-x="110" data-y="65" data-speed="300" data-start="900" data-end="2800" data-easing="easeOutBack"><img src="images/slides/image2_caption-2.png"></div>
+							</li>
+							
+							<!-- second set of thumbnails -->
+							<li data-transition="fade" data-delay="2000" data-masterspeed="0">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image8.jpg"/></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="400" data-y="140" data-speed="300" data-start="200" data-easing="easeOutBack"><img src="images/slides/p4.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="560" data-y="140" data-speed="300" data-start="400" data-easing="easeOutBack"><img src="images/slides/p5.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="720" data-y="140" data-speed="300" data-start="600" data-easing="easeOutBack"><img src="images/slides/p6.jpg"></div>
+							</li>
+							
+							<li data-transition="fade" data-slotamount="0" data-masterspeed="0">
+								<img src="images/slides/image9.jpg"/>
+								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
+							</li>
+							
+							<li data-transition="fade" data-slotamount="0" data-masterspeed="0">
+								<img src="images/slides/image5.jpg"/>
+								<div class="caption fade" data-x="0" data-y="0" data-speed="150" data-start="0">&nbsp;</div>
+							</li>
+							
+							<!-- second caption -->
+							<li data-transition="fade" data-delay="4000" data-masterspeed="0">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image3.jpg"/></div>
 								<div class="caption sfr"  data-x="580" data-y="100" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-we-match-1.png"/></div>
 								<div class="caption sfr"  data-x="580" data-y="140" data-speed="900" data-start="900" data-easing="easeOutBack"><img src="images/slides/caption-we-match-2.png"/></div>
 								<div class="caption sfr"  data-x="630" data-y="195" data-speed="900" data-start="1100" data-easing="easeOutBack"><img src="images/slides/caption-we-match-3.png"/></div>
 								<div class="caption sfr"  data-x="690" data-y="240" data-speed="900" data-start="1300" data-easing="easeOutBack"><img src="images/slides/caption-we-match-4.png"/></div>
 							</li>
 							
-							<!-- curtain transition -->
-							<li data-transition="curtain-3" data-delay="5000" data-slotamount="0" data-masterspeed="500"  data-thumb="images/thumbs/regular_thumb2.jpg">
-								<img src="images/slides/image6.jpg"/>
+							<!-- third set of thumbnails -->
+							<li data-transition="fade" data-delay="2000" data-masterspeed="0">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image14.jpg"/></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="400" data-y="140" data-speed="300" data-start="200" data-easing="easeOutBack"><img src="images/slides/p7.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="560" data-y="140" data-speed="300" data-start="400" data-easing="easeOutBack"><img src="images/slides/p8.jpg"></div>
+                                <div class="caption white-bg randomrotate lfr" data-x="720" data-y="140" data-speed="300" data-start="600" data-easing="easeOutBack"><img src="images/slides/p9.jpg"></div>
+							</li>
+							
+							<!-- third caption -->
+							<li data-transition="curtain-3" data-delay="2500" data-masterspeed="0">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image6.jpg"/></div>
 								<div class="caption sfl" data-x="35" data-y="80" data-speed="900" data-start="700" data-end="1000" data-easing="easeOutBack"><img src="images/slides/caption-savemoney-1.png"/></div>
 								<div class="caption sfl" data-x="125" data-y="130" data-speed="900" data-start="900" data-easing="easeOutBack"><img src="images/slides/caption-savemoney-2.png"/></div>
 							</li>
-
-							<!-- cubic transition -->
-							<li data-transition="curtain-2" data-slotamount="0" data-masterspeed="1000"  data-thumb="images/thumbs/regular_thumb2.jpg">
-								<img src="images/slides/image5.jpg"/>
-							</li>
 							
-							<li data-transition="curtain-2" data-slotamount="0" data-masterspeed="500"  data-thumb="images/thumbs/regular_thumb2.jpg">
-								<img src="images/slides/image7.jpg"/>
+							<!-- fourth caption -->
+							<li data-transition="fade" data-delay="4000" data-masterspeed="0">
+								
+								<div class="caption fade" data-x="0" data-y="0" data-speed="50" data-start="0"><img src="images/slides/image7.jpg"/></div>
 								<div class="caption sfr"  data-x="580" data-y="100" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/caption-we-match-1.png"/></div>
 								<div class="caption sfr"  data-x="580" data-y="140" data-speed="900" data-start="900" data-easing="easeOutBack"><img src="images/slides/caption-we-match-2.png"/></div>
 								<div class="caption sfr"  data-x="630" data-y="195" data-speed="900" data-start="1100" data-easing="easeOutBack"><img src="images/slides/caption-we-match-3.png"/></div>
 								<div class="caption sfr"  data-x="690" data-y="240" data-speed="900" data-start="1300" data-easing="easeOutBack"><img src="images/slides/caption-we-match-4.png"/></div>
 							</li>
+
+							<!-- basic image flashing -->
+							<li data-transition="fade" data-delay="700" data-masterspeed="0">
+								<img src="images/slides/image5.jpg"/>
+							</li>
 							
-							<li data-transition="curtain-2" data-slotamount="0" data-masterspeed="1000"  data-thumb="images/thumbs/regular_thumb2.jpg">
+							<li data-transition="fade" data-slotamount="0" data-masterspeed="1000">
 								<img src="images/slides/image8.jpg"/>
 							</li>
 							
-							<li data-transition="curtain-3" data-slotamount="0" data-masterspeed="500"  data-thumb="images/thumbs/regular_thumb2.jpg">
+							<li data-transition="fade" data-slotamount="0" data-masterspeed="500">
 								<img src="images/slides/image9.jpg"/>
 							</li>
 							
-							<li data-transition="papercut" data-delay="5000" data-slotamount="0" data-masterspeed="400"  data-thumb="images/thumbs/regular_thumb2.jpg">
+							<li data-transition="fade" data-delay="5000" data-slotamount="0" data-masterspeed="400">
 								<img src="images/slides/image12.jpg"/>
                                 <div class="caption lfl" data-x="130" data-y="200" data-speed="900" data-start="700" data-easing="easeOutBack"><img src="images/slides/image2_caption-1.png"></div>
                                 <div class="caption lfb" data-x="500" data-y="200" data-speed="900" data-start="1500" data-easing="easeOutBack"><img src="images/slides/image2_caption-2.png"></div>
 							</li>
 							
-							<li data-transition="slidedown" data-slotamount="0" data-masterspeed="400"  data-thumb="images/thumbs/regular_thumb2.jpg">
+							<li data-transition="slidedown" data-slotamount="0" data-masterspeed="400">
 								<img src="images/slides/image11.jpg"/>
 							</li>
 							
-							<?php /*
-							<div class="caption lfb big_white"  data-x="400" data-y="100" data-speed="900" data-start="1700" data-easing="easeOutBack">Kickstart Your Website</div>
-							<div class="caption lft big_orange"  data-x="400" data-y="137" data-speed="900" data-start="1900" data-easing="easeOutBack"><span style="font-weight:normal;">With</span><em> Slider Revolution</em></div>
-							<div class="caption lfr medium_grey"  data-x="510" data-y="210" data-speed="300" data-start="2500" data-easing="easeOutExpo">Unlimited Transitions</div>
-							
-                            <div class="caption sfl" data-x="510" data-y="250" data-speed="300" data-start="2600" data-easing="easeOutExpo"><img src="images/TILES/check.png"></div>
-                            <div class="caption lfr small_text"  data-x="560" data-y="248" data-speed="300" data-start="2600" data-easing="easeOutExpo">Sliding, Fading, Slots, Box Transitions<br/>
-							<span style="color: #ffe400;">SLIDER REVOLUTION</span> has it all!</div>
-							<div class="caption sfl" data-x="510" data-y="300" data-speed="300" data-start="2900" data-easing="easeOutExpo"><img src="images/TILES/check.png"></div>
-                            <div class="caption lfr small_text"  data-x="560" data-y="307" data-speed="300" data-start="2900" data-easing="easeOutExpo">Responsive &bsp; Mobile Optimized</div>
-                            <div class="caption sfl" data-x="510" data-y="350" data-speed="300" data-start="3200" data-easing="easeOutExpo"><img src="images/TILES/check.png"></div>
-                            <div class="caption lfr small_text"  data-x="560" data-y="348" data-speed="300" data-start="3200" data-easing="easeOutExpo">Customizable Navigation<br/>Arrows, Bullets, Thumbs</div>
-							*/ ?>
-
-							<?php /*
-							<!-- SLIDE DOWN -->
-							<li data-transition="slidedown" data-slotamount="1" data-thumb="images/thumbs/regular_thumb3.jpg">
-								<img src="images/slides/image3.jpg"  >
-                                <div class="caption sfb" data-x="0" data-y="380" data-speed="900" data-start="500" data-easing="easeOutBack"><img src="images/slides/floor.png"></div>
-                                <div class="caption lfr" data-x="500" data-y="20" data-speed="700" data-start="800" data-easing="easeOutExpo"><img src="images/slides/dude1.png"></div>
-                                <div class="caption lfr" data-x="550" data-y="20" data-speed="700" data-start="1000" data-easing="easeOutExpo"><img src="images/slides/girl1.png"></div>
-                                <div class="caption lfl very_big_white"  data-x="100" data-y="100" data-speed="300" data-start="1200" data-easing="easeOutExpo">GET THE</div>
-                                <div class="caption lfl very_big_white"  data-x="100" data-y="160" data-speed="300" data-start="1300" data-easing="easeOutExpo">PARTY</div>
-                                <div class="caption lfl very_big_white"  data-x="100" data-y="220" data-speed="300" data-start="1400" data-easing="easeOutExpo">STARTED</div>
-                                <div class="caption lfb big_white"  data-x="100" data-y="300" data-speed="300" data-start="1500" data-easing="easeOutExpo"><a href="#">SIGN UP HERE</a></div>
-							</li>
-
-							<!-- SLOTFADE HORIZONTAL -->
-							<li data-transition="slotfade-horizontal" data-slotamount="20" data-thumb="images/thumbs/regular_thumb4.jpg">
-								<img src="images/slides/image18.jpg"  >
-                                <div class="caption lft big_white"  data-x="100" data-y="70" data-speed="1200" data-start="800" data-easing="easeOutBack">American Muscle Car</div>
-                                <div class="caption lfl medium_grey"  data-x="270" data-y="180" data-speed="900" data-start="1200" data-easing="easeOutExpo">4.6l V8 Engine</div>
-                                <div class="caption lfr medium_grey"  data-x="480" data-y="330" data-speed="900" data-start="1400" data-easing="easeOutExpo">20 inch Rims</div>
-							</li>
-
-							<!-- SLIDE UP -->
-							<li data-transition="slideup" data-slotamount="20" data-delay="25000" data-thumb="images/thumbs/regular_thumb5.jpg">
-								<img src="images/slides/image20.jpg"  >
-                                <div class="caption lfb boxshadow" data-x="70" data-y="120" data-speed="900" data-start="500" data-easing="easeOutBack"><iframe src="http://player.vimeo.com/video/29298709?title=0&amp;byline=0&amp;portrait=0" width="460" height="259"></iframe></div>
-                                <div class="caption sft big_black"  data-x="550" data-y="120" data-speed="300" data-start="1200" data-easing="easeOutExpo">Video Support</div>
-                                <div class="caption sft big_white"  data-x="550" data-y="157" data-speed="300" data-start="1300" data-easing="easeOutExpo">Vimeo Example</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="210" data-speed="300" data-start="1400" data-easing="easeOutExpo">You can easily add</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="234" data-speed="300" data-start="1500" data-easing="easeOutExpo">Vimeo & Youtube Videos</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="258" data-speed="300" data-start="1600" data-easing="easeOutExpo">to your Slides</div>
-							</li>
-
-                            <!-- SLOTZOOM VERTICAL -->
-							<li data-transition="slotzoom-vertical" data-slotamount="14" data-delay="25000" data-thumb="images/thumbs/regular_thumb6.jpg">
-								<img src="images/slides/image22.jpg"  >
-                                <div class="caption lft boxshadow" data-x="70" data-y="120" data-speed="900" data-start="500" data-easing="easeOutBack"><iframe src="http://www.youtube.com/embed/YHWkro9-e9Q?hd=1&amp;wmode=opaque&amp;controls=1&amp;showinfo=0" width="460" height="259"></iframe></div>
-                                <div class="caption sft big_black"  data-x="550" data-y="120" data-speed="300" data-start="1200" data-easing="easeOutExpo">Video Support</div>
-                                <div class="caption sft big_white"  data-x="550" data-y="157" data-speed="300" data-start="1300" data-easing="easeOutExpo">Youtube Example</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="210" data-speed="300" data-start="1400" data-easing="easeOutExpo">You can easily add</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="234" data-speed="300" data-start="1500" data-easing="easeOutExpo">Vimeo & Youtube Videos</div>
-                                <div class="caption lfb medium_grey"  data-x="550" data-y="258" data-speed="300" data-start="1600" data-easing="easeOutExpo">to your Slides</div>
-							</li>
-
-							<!-- CURTAIN 3 -->
-							<li data-transition="curtain-3" data-slotamount="20" data-thumb="images/thumbs/regular_thumb7.jpg">
-								<img src="images/slides/image21.jpg"  >
-                                <div class="caption lft" data-x="330" data-y="156" data-speed="900" data-start="500" data-easing="easeOutExpo"><img src="images/slides/burger.png"></div>
-                                <div class="caption lft" data-x="450" data-y="18" data-speed="900" data-start="700" data-easing="easeOutExpo"><img src="images/slides/coke.png"></div>
-                                <div class="caption sft big_orange"  data-x="80" data-y="340" data-speed="900" data-start="1200" data-easing="easeOutBack">Serving you the Ultimate Slider</div>
-                                <div class="caption sft medium_text black"  data-x="85" data-y="385" data-speed="900" data-start="1300" data-easing="easeOutBack">Get Slider Revolution today!</div>
-							</li>
-							*/ ?>
 						</ul>
 						<div class="tp-bannertimer"></div>
 					</div>
 				</div>
-
-				<div style="height:50px"></div>
-
+				
 			<script type="text/javascript">
 
 				var tpj=jQuery;
